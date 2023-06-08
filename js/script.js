9e1c0e53f66fcefac643eb719088b64f2cde2286
osama-sayed3344 <osamasayed3344@gmail.com>
2023-06-08 18:13:12 +0300

    update data

--- js/script.js
+++ js/script.js
@@ -11,24 +11,6 @@ function myfunction(){
 function display_logo(){
 	document.documentElement.scrollTop=0;
 }
-function display_about(){
-	document.documentElement.scrollTop=263;
-}
-function display_skills(){
-	document.documentElement.scrollTop=600;
-}
-function display_Experience(){
-	document.documentElement.scrollTop=1100;
-}
-function display_Education(){
-	document.documentElement.scrollTop=1600;
-}
-function display_Portfolio(){
-	document.documentElement.scrollTop=2100;
-}
-function display_contact(){
-	document.documentElement.scrollTop=3100;
-}
 /*----------------------------------------------------------------*/
 function funclick(){
 	document.querySelector("ul").classList.toggle("show_menu");
